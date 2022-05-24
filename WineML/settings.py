b8c1617a5bb13dbcbc9de9d0759d5922ef9b627c
troye <troyegilbert@pm.me>
2022-05-24 15:28:54 +0300

    Automatic configuration

--- WineML/settings.py
+++ WineML/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-$wb58h9se9bz(1556!bq0e&rdlhxchmhe%48z9^59&7px++htl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
